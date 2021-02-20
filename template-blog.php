0b26ba951c7f4398f17e6087619ad83fefd6fe1c
AleksRomanov <kosmonavt-vavilon@mail.ru>
2021-02-21 02:17:04 +0700

    Frontend Blog Page rework

--- template-blog.php
+++ template-blog.php
@@ -10,47 +10,38 @@ get_header();
 
 <main>
   <section class="blog">
-    <div class="blog-container container">
-      <div class="blog-header-links">
-        <ul>
-          <li><a class="link-active" href="#">Новости</a></li>
-          <li><a href="#">Стартапы</a></li>
-          <li><a href="#">Интервью</a></li>
-        </ul>
-      </div>
-
-
+    <div class="container">
+      <ul class="blog-links-list">
+        <li class="blog-links-list__item">
+          <a class="blog-links-list__link blog-links-list__link--active" href="#">Новости</a>
+        </li>
+        <li class="blog-links-list__item">
+          <a class="blog-links-list__link" href="#">Стартапы</a>
+        </li>
+        <li class="blog-links-list__item">
+          <a class="blog-links-list__link" href="#">Интервью</a>
+        </li>
+      </ul>
 
       <div class="blog-content">
-        <ul class="blog-content-list">
-          <li class="blog-content-item">
-            <div class="item-description">
-              <h2>Шаурма "По-Братски"</h2>
-              <span>Мобильное приложение для онлайн-заказа, с собственной панелью управления и возможностью собирать отзывы клиентов.</span>
-            </div>
+        <ul class="blog-content__list">
+          <li class="blog-content__item">
+            <img class="blog-content__image" src="../wp-content/themes/js-corp/images/blog/back_item.jpg" width="190" height="190" alt="background">
+            <h2 class="blog-content__title">Шаурма "По-Братски"</h2>
+            <span class="blog-content__info">Мобильное приложение для онлайн-заказа, с собственной панелью управления и возможностью собирать отзывы клиентов.</span>
           </li>
-          <li class="blog-content-item">
-            <div class="item-description">
-              <h2>Шаурма "По-Братски"</h2>
-              <span>Мобильное приложение для онлайн-заказа, с собственной панелью управления и возможностью собирать отзывы клиентов.</span>
-            </div>
+          <li class="blog-content__item">
+            <img class="blog-content__image" src="../wp-content/themes/js-corp/images/blog/back_item.jpg" width="190" height="190" alt="background">
+            <h2 class="blog-content__title">Шаурма "По-Братски"</h2>
+            <span class="blog-content__info">Мобильное приложение для онлайн-заказа, с собственной панелью управления и возможностью собирать отзывы клиентов.</span>
           </li>
-          <li class="blog-content-item">
-            <div class="item-description">
-              <h2>Шаурма "По-Братски"</h2>
-              <span>Мобильное приложение для онлайн-заказа, с собственной панелью управления и возможностью собирать отзывы клиентов.</span>
-            </div>
+          <li class="blog-content__item">
+            <img class="blog-content__image" src="../wp-content/themes/js-corp/images/blog/back_item.jpg" width="190" height="190" alt="background">
+            <h2 class="blog-content__title">Шаурма "По-Братски"</h2>
+            <span class="blog-content__info">Мобильное приложение для онлайн-заказа, с собственной панелью управления и возможностью собирать отзывы клиентов.</span>
           </li>
         </ul>
-        <div class="blog-content-list--pages">
-          <ul>
-            <li class="page-active">1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <span>Показать еще</span>
-          </ul>
-        </div>
+        <div id="true_loadmore" class="btn showmore__btn">Показать еще</div>
       </div>
 
     </div>
